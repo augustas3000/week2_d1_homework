f8ae047260ce6bbe11ea7b6fbd312f02ec9693e8
augustas3000 <augusto.pasaulis@gmail.com>
2020-01-13 16:43:30 +0000

    Extension for homework, draft finished

--- classes.rb
+++ classes.rb
@@ -57,3 +57,123 @@ class Team
   end
 
 end
+
+
+# Create a class for a Library that has an array of books. Each book should be a hash with a title, which is a string, and rental details, which is another hash with a student name and due date
+
+
+class Library
+
+  attr_reader :books
+  # attr_writer :
+
+
+  def initialize
+    @books = [
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+        student_name: "Jeff",
+        date: "01/12/16"
+        }
+      },
+      {
+        title: "cats_cradle",
+        rental_details: {
+        student_name: "Judy",
+        date: "01/12/19"
+        }
+      },
+      {
+        title: "Necronomicon",
+        rental_details: {
+        student_name: "Erica",
+        date: "01/12/15"
+        }
+      }
+    ]
+  end
+
+  # Create a method that takes in a book title and returns all of the information about that book.
+
+  def return_book_hash_simple(book_title_str)
+    for book_hash in @books
+      if book_hash[:title] == book_title_str
+        return  book_hash
+      end
+    end
+    return "The book is not available"
+  end
+
+
+  def find_info_by_title(book_title_str)
+
+    for book_hash in @books
+
+      if book_hash[:title] == book_title_str
+        return "Title is: #{book_hash[:title]}\n,
+                Rental details:\n
+                Student Name: #{book_hash[:rental_details][:student_name]},\n
+                Due Date: #{book_hash[:rental_details][:date]}"
+      end
+
+    end
+
+    return "The book is not available"
+  end
+
+  # Create a method that takes in a book title and returns only the rental details for that book.
+
+  def find_rental_info_by_title(book_title_str)
+
+    for book_hash in @books
+
+      if book_hash[:title] == book_title_str
+        return "Rental details:\n
+                Student Name: #{book_hash[:rental_details][:student_name]},\n
+                Due Date: #{book_hash[:rental_details][:date]}"
+      end
+
+    end
+
+    return "The book is not available"
+  end
+
+# Create a method that takes in a book title and adds it to our book list (add a new hash for the book with the student name and date being left as empty strings)
+
+  def add_book(book_title_str)
+    # is the book not already present in @books list?
+    for book_hash in @books
+      if book_hash[:title] == book_title_str
+        return "Such book is already in the library"
+      end
+    end
+
+    new_book = {
+            title: book_title_str,
+            rental_details: {
+            student_name: "",
+            date: ""
+            }}
+
+    @books.push(new_book)
+
+  end
+
+
+  # Create a method that changes the rental details of a book by taking in the title of the book, the student renting it and the date it’s due to be returned.
+
+  def change_rent_details(book_title_str, student_str, due_date_str)
+    # return hash of questioned book if present and save it
+    # to a variable book_hash
+    book_hash = return_book_hash_simple(book_title_str)
+    # delete the old one from @books array
+    @books.delete(book_hash)
+    # update saved hash as required
+    book_hash[:rental_details][:student_name] = student_str
+    book_hash[:rental_details][:date] = due_date_str
+    # push the updated book hash to @books array
+    @books.push(book_hash)
+  end
+
+end
--- spec/classes_spec.rb
+++ spec/classes_spec.rb
@@ -71,3 +71,104 @@ class TestTeam < Minitest::Test
   end
 
 end
+
+
+class TestLibrary < Minitest::Test
+  def setup
+    @library_obj = Library.new
+    # define a local variable @new_book to store a new book hash
+    # this is for testing add_book method
+    @new_book = {
+            title: "Killing Floor",
+            rental_details: {
+            student_name: "",
+            date: ""
+            }}
+    # for checking change_rent_details method:
+    @updated_book = {
+            title: "lord_of_the_rings",
+            rental_details: {
+            student_name: "Josh",
+            date: "01/12/12"
+            }}
+
+  end
+
+  def test_get_books
+    assert([
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+        student_name: "Jeff",
+        date: "01/12/16"
+        }
+      },
+      {
+        title: "cats_cradle",
+        rental_details: {
+        student_name: "Judy",
+        date: "01/12/19"
+        }
+      },
+      {
+        title: "Necronomicon",
+        rental_details: {
+        student_name: "Erica",
+        date: "01/12/15"
+        }
+      }
+    ], @library_obj.books)
+  end
+
+  def test_find_info_by_title_book_avilable()
+    actual = @library_obj.find_info_by_title("Necronomicon")
+    expected = "Title is: Necronomicon\n,
+                Rental details:\n
+                Student Name: Erica,\n
+                Due Date: 01/12/15"
+    assert_equal(expected, actual)
+  end
+
+
+  def test_find_info_by_title_book_not_avilable()
+    actual = @library_obj.find_info_by_title("Fear and Loath in Las vegas")
+    expected = "The book is not available"
+    assert_equal(expected, actual)
+  end
+
+
+  def test_find_rental_info_by_title_book_avilable()
+    actual = @library_obj.find_rental_info_by_title("Necronomicon")
+    expected = "Rental details:\n
+                Student Name: Erica,\n
+                Due Date: 01/12/15"
+
+    assert_equal(expected, actual)
+  end
+
+
+  def test_find_rental_info_by_title_book_not_avilable()
+    actual = @library_obj.find_rental_info_by_title("Fear and Loath in Las vegas")
+    expected = "The book is not available"
+    assert_equal(expected, actual)
+  end
+
+  def test_add_book_book_not_in_lib()
+    @library_obj.add_book("Killing Floor")
+    actual = @library_obj.return_book_hash_simple("Killing Floor")
+    assert_equal(@new_book, actual)
+  end
+
+
+  def test_add_book_book_in_lib()
+    assert_equal("Such book is already in the library", @library_obj.add_book("Necronomicon"))
+  end
+
+  def test_change_rent_details
+    # change details:
+    @library_obj.change_rent_details("lord_of_the_rings", "Josh", "01/12/12" )
+    expected = @updated_book
+    actual = @library_obj.return_book_hash_simple("lord_of_the_rings")
+    assert_equal(expected, actual)
+  end
+end
